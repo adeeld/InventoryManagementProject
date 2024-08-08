1b4a372728bde27eb98b509f6a50c674ccfa9c87
Adeel Durrani <jeoydurrani2003@gmail.com>
2024-08-08 01:55:20 -0400

    Configured environment variables

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const firebaseConfig = {
     apiKey: "AIzaSyApbwZOYA7sVIQ4X6SQDGFBHukx8IYHq5g",
     authDomain: "inventorymanagement-fc275.firebaseapp.com",
@@ -8,6 +12,8 @@ const firebaseConfig = {
     messagingSenderId: "728954031055",
     appId: "1:728954031055:web:174382eb2390969139e0ba"
  };
+
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+
 export { firestore };
