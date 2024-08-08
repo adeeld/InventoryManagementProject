67433c844b33dd67fc22ba0d243d09959dce7020
Adeel Durrani <jeoydurrani2003@gmail.com>
2024-08-08 01:32:23 -0400

    first commit - setting up firebase and next js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyApbwZOYA7sVIQ4X6SQDGFBHukx8IYHq5g",
+    authDomain: "inventorymanagement-fc275.firebaseapp.com",
+    projectId: "inventorymanagement-fc275",
+    storageBucket: "inventorymanagement-fc275.appspot.com",
+    messagingSenderId: "728954031055",
+    appId: "1:728954031055:web:174382eb2390969139e0ba"
+ };
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
